1c9a79ad964af1803b79f9439c48dd31707a781a
RonitPatil2005 <ronitrpatil16@gamil.com>
2026-06-10 12:39:07 +0530

    Fix Render deployment and navbar

--- app.js
+++ app.js
@@ -1,80 +1,96 @@
-if(process.env.NODE_ENV != "production") {
-    require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
 }
 
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
+
 const mongoose = require("mongoose");
 const path = require("path");
-const methodOvrride = require("method-override");
+const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+
 const session = require("express-session");
 const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
-const passport = require("passport"); 
+
+const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user.js");
-const {isLoggedIn} = require("./middleware.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+// ----------------------
+// View Engine Setup
+// ----------------------
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOvrride("_method"));
-app.engine("ejs", ejsMate); 
-app.use(express.static(path.join(__dirname,"/public")));
+app.set("views", path.join(__dirname, "views"));
 
+app.engine("ejs", ejsMate);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// ----------------------
+// MongoDB Connection
+// ----------------------
 const dbUrl = process.env.ATLASDB_URL;
-main().then(() => {
-    console.log("Connected to db");
-}).catch(err => {
-    console.log(err);
-});
 
 async function main() {
     await mongoose.connect(dbUrl);
 }
 
+// ----------------------
+// Session Store
+// ----------------------
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
-    touchAfter: 24 * 3600, // seconds
+    touchAfter: 24 * 3600,
 });
 
 store.on("error", (err) => {
-    console.log("ERROR IN MONGO SESSION STORE", err);
+    console.log("SESSION STORE ERROR:", err);
 });
 
 const sessionOptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-    }, 
+    },
 };
 
 app.use(session(sessionOptions));
 app.use(flash());
 
+// ----------------------
+// Passport Setup
+// ----------------------
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next) => {
+// ----------------------
+// Global Variables
+// ----------------------
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
@@ -82,21 +98,45 @@ app.use((req,res,next) => {
     next();
 });
 
+// ----------------------
+// Routes
+// ----------------------
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// 404 handler
-app.use((req, res, next) => {
+// ----------------------
+// 404 Handler
+// ----------------------
+app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
 });
 
+// ----------------------
+// Error Handler
+// ----------------------
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
+    let { statusCode = 500 } = err;
+
+    if (!err.message) {
+        err.message = "Something Went Wrong!";
+    }
+
     res.status(statusCode).render("error.ejs", { err });
 });
 
-app.listen(port, () => {
-    console.log("App is listen");
-});
+// ----------------------
+// Start Server AFTER DB Connects
+// ----------------------
+main()
+    .then(() => {
+        console.log("Connected to MongoDB");
 
+        app.listen(port, () => {
+            console.log(`Server listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection Error");
+        console.log(err);
+    });
